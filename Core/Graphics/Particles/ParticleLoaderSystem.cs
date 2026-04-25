4df625404eb566187c4e6e4eb32c29c381afa4f7
Bread <gamergreen742@gmail.com>
2026-04-24 18:35:38 -0600

    Fixed an issue relating to malformed dependencies

--- Core/Graphics/Particles/ParticleLoaderSystem.cs
+++ Core/Graphics/Particles/ParticleLoaderSystem.cs
@@ -17,28 +17,69 @@ namespace BreadLibrary.Core.Graphics.Particles
                 if (assembly is null)
                     continue;
 
-
-
-                foreach (Type type in assembly.GetTypes())
+                foreach (Type type in GetLoadableTypes(assembly, mod))
                 {
-                    if (type.IsAbstract)
+                    if (type is null)
+                        continue;
+
+                    if (type.IsAbstract || type.IsInterface)
                         continue;
 
                     if (!typeof(IBaseParticle).IsAssignableFrom(type))
                         continue;
 
-                    if (InitializedParticleTypes.Contains(type))
+                    if (!InitializedParticleTypes.Add(type))
                         continue;
 
-                    if (Activator.CreateInstance(type) is not IBaseParticle particle)
+                    IBaseParticle particle;
+
+                    try
+                    {
+                        particle = (IBaseParticle)Activator.CreateInstance(type);
+                    }
+                    catch (Exception ex)
+                    {
+                        Mod.Logger.Warn($"Failed to create particle type '{type.FullName}' from mod '{mod.Name}'.", ex);
                         continue;
+                    }
 
-                    particle.SetStaticDefaults();
-                    InitializedParticleTypes.Add(type);
-
+                    try
+                    {
+                        particle.SetStaticDefaults();
+                    }
+                    catch (Exception ex)
+                    {
+                        Mod.Logger.Warn($"Failed to run SetStaticDefaults for particle type '{type.FullName}' from mod '{mod.Name}'.", ex);
+                    }
                 }
             }
         }
+
+        private IEnumerable<Type> GetLoadableTypes(Assembly assembly, Mod ownerMod)
+        {
+            try
+            {
+                return assembly.GetTypes();
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                foreach (Exception loaderException in ex.LoaderExceptions.Where(e => e is not null))
+                {
+                    Mod.Logger.Warn(
+                        $"Could not load one or more types from mod '{ownerMod.Name}' while scanning for particles: " +
+                        loaderException.Message
+                    );
+                }
+
+                return ex.Types.Where(t => t is not null)!;
+            }
+            catch (Exception ex)
+            {
+                Mod.Logger.Warn($"Failed to scan assembly for mod '{ownerMod.Name}'.", ex);
+                return Enumerable.Empty<Type>();
+            }
+        }
+
 
         public override void Unload()
         {
