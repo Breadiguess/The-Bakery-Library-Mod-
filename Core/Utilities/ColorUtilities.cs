918133abbae3016253e8e38a2c34feafb9ac1619
Bread <97198020+Breadiguess@users.noreply.github.com>
2026-04-25 11:42:54 -0600

    Update ColorUtilities to not be empty
    Hi guys

--- Core/Utilities/ColorUtilities.cs
+++ Core/Utilities/ColorUtilities.cs
@@ -1,1 +1,74 @@
-a
+using Microsoft.Xna.Framework;
+
+namespace BreadLibrary.Core.Utilities
+
+{
+
+    public static partial class Utilities
+
+    {
+
+        /// <summary>
+
+        /// Converts a normalized Vector3 color value into a Color.
+
+        /// 
+
+        /// X = R, Y = G, Z = B.
+
+        /// Alpha is assumed to be fully opaque.
+
+        /// Expected component range is 0f to 1f.
+
+        /// </summary>
+
+        public static Color ToColor(this Vector3 vector)
+
+        {
+
+            return new Color(
+
+                MathHelper.Clamp(vector.X, 0f, 1f),
+
+                MathHelper.Clamp(vector.Y, 0f, 1f),
+
+                MathHelper.Clamp(vector.Z, 0f, 1f),
+
+                1f
+
+            );
+
+        }
+
+        /// <summary>
+
+        /// Converts a normalized Vector4 color value into a Color.
+
+        /// 
+
+        /// X = R, Y = G, Z = B, W = A.
+
+        /// Expected component range is 0f to 1f.
+
+        /// </summary>
+
+        public static Color ToColor(this Vector4 vector)
+
+        {
+
+            return new Color(
+
+                MathHelper.Clamp(vector.X, 0f, 1f),
+
+                MathHelper.Clamp(vector.Y, 0f, 1f),
+
+                MathHelper.Clamp(vector.Z, 0f, 1f),
+
+                MathHelper.Clamp(vector.W, 0f, 1f)
+
+            );
+
+        }
+
+     }
+}
